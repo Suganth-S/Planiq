6ab2c1a56d583203e4871e9f2da22dfd7bc00e7d
Suganth <suganth.sakthivel@ideas2it.com>
2024-10-19 18:15:32 +0530

    Dependency injection integrated

---
+++ app/src/main/java/com/example/todoappjetpackcompose/ui/viewmodels/SharedViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.todoappjetpackcompose.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoappjetpackcompose.data.models.ToDoTask
+import com.example.todoappjetpackcompose.data.repositories.ToDoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+    private val repository: ToDoRepository
+): ViewModel() {
+
+    private val _allTasks = MutableStateFlow<List<ToDoTask>>(emptyList())
+    val allTasks : StateFlow<List<ToDoTask>> = _allTasks
+
+    fun getAllTasks() {
+        viewModelScope.launch {
+            repository.getAllTask.collect{
+                _allTasks.value = it
+            }
+        }
+    }
+
+
+}
