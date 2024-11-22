79dc38007ba7df932b2f648ad4217769f574ba00
Suganth <suganth.sakthivel@ideas2it.com>
2024-11-22 21:12:17 +0530

    Swipe to delete feature integration

--- app/src/main/java/com/example/todoappjetpackcompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/example/todoappjetpackcompose/ui/screens/list/ListContent.kt
@@ -1,5 +1,11 @@
 package com.example.todoappjetpackcompose.ui.screens.list
 
+import android.graphics.drawable.Icon
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -8,34 +14,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import com.example.todoappjetpackcompose.R
 import com.example.todoappjetpackcompose.data.models.Priority
 import com.example.todoappjetpackcompose.data.models.ToDoTask
 import com.example.todoappjetpackcompose.ui.theme.HighPriorityColor
@@ -47,6 +42,24 @@ import com.example.todoappjetpackcompose.ui.theme.taskItemBackgroundColor
 import com.example.todoappjetpackcompose.ui.theme.taskItemTextColor
 import com.example.todoappjetpackcompose.util.RequestState
 import com.example.todoappjetpackcompose.util.SearchAppbarState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.rememberSwipeToDismissBoxState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.todoappjetpackcompose.R
+import com.example.todoappjetpackcompose.util.Action
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ListContent(
@@ -57,6 +70,7 @@ fun ListContent(
     sortStates: RequestState<Priority>,
     contentPaddingValues: PaddingValues,
     searchAppbarState: SearchAppbarState,
+    onSwipeToDelete: (Action, ToDoTask) -> Unit,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     if (sortStates is RequestState.Success) {
@@ -66,6 +80,7 @@ fun ListContent(
                     HandleListContent(
                         task = searchedTasks.data,
                         contentPaddingValues = contentPaddingValues,
+                        onSwipeToDelete = onSwipeToDelete,
                         navigateToTaskScreen = navigateToTaskScreen
                     )
                 }
@@ -76,6 +91,7 @@ fun ListContent(
                     HandleListContent(
                         task = tasks.data,
                         contentPaddingValues = contentPaddingValues,
+                        onSwipeToDelete = onSwipeToDelete,
                         navigateToTaskScreen = navigateToTaskScreen
                     )
                 }
@@ -85,6 +101,7 @@ fun ListContent(
                 HandleListContent(
                     task = lowPriorityTasks,
                     contentPaddingValues = contentPaddingValues,
+                    onSwipeToDelete= onSwipeToDelete,
                     navigateToTaskScreen = navigateToTaskScreen
                 )
             }
@@ -92,6 +109,7 @@ fun ListContent(
                 HandleListContent(
                     task = highPriorityTasks,
                     contentPaddingValues = contentPaddingValues,
+                    onSwipeToDelete = onSwipeToDelete,
                     navigateToTaskScreen = navigateToTaskScreen
                 )
             }
@@ -103,6 +121,7 @@ fun ListContent(
 fun HandleListContent(
     task: List<ToDoTask>,
     contentPaddingValues: PaddingValues,
+    onSwipeToDelete: (Action, ToDoTask) -> Unit,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     if (task.isEmpty()){
@@ -111,6 +130,7 @@ fun HandleListContent(
         DisplayTasks(
             tasks = task,
             contentPaddingValues = contentPaddingValues,
+            onSwipeToDelete = onSwipeToDelete,
             navigateToTaskScreen = navigateToTaskScreen
         )
     }
@@ -121,6 +141,7 @@ fun HandleListContent(
 fun DisplayTasks(
     tasks: List<ToDoTask>,
     contentPaddingValues: PaddingValues,
+    onSwipeToDelete: (Action, ToDoTask) -> Unit,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     LazyColumn(contentPadding = contentPaddingValues){
@@ -132,8 +153,67 @@ fun DisplayTasks(
                 task.id
             }
         ){task ->
-            TaskItem(toDoTask = task, navigateToTaskScreen = navigateToTaskScreen)
+            val dismissState = rememberSwipeToDismissBoxState()
+            val dismissDirection = dismissState.dismissDirection
+            val isDismissed = dismissState.dismissDirection == SwipeToDismissBoxValue.EndToStart && dismissState.progress == 1f
+            if (isDismissed && dismissDirection == SwipeToDismissBoxValue.EndToStart){
+                val scope = rememberCoroutineScope()
+                SideEffect {
+                    scope.launch {
+                        delay(300)
+                        onSwipeToDelete(Action.DELETE, task)
+                    }
+                }
+            }
+            val degree by animateFloatAsState(
+                targetValue = if (dismissState.progress in 0f..0.5f) 0f else -45f,
+                label = "Degree Animation"
+            )
+
+            var itemAppeared by remember {
+                mutableStateOf(false)
+            }
+            LaunchedEffect(key1 = true ){
+                itemAppeared = true
+            }
+
+            AnimatedVisibility(
+                visible = itemAppeared && !isDismissed,
+                enter = expandVertically(
+                    animationSpec = tween(
+                        durationMillis = 300
+                    )
+                ),
+                exit = shrinkVertically(
+                    animationSpec = tween(
+                        durationMillis = 300
+                    )
+                )
+            ) {
+                SwipeToDismissBox(
+                    state = dismissState,
+                    backgroundContent = { RedBackground(degrees = degree) }) {
+                    TaskItem(toDoTask = task, navigateToTaskScreen = navigateToTaskScreen)
+                }
+            }
         }
+    }
+}
+
+
+@Composable
+fun RedBackground(degrees: Float) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(HighPriorityColor)
+        .padding(horizontal = LARGEST_PADDING),
+        contentAlignment = Alignment.CenterEnd) {
+        Icon(
+            modifier = Modifier.rotate(degrees),
+            imageVector = Icons.Filled.Delete,
+            contentDescription = stringResource(id = R.string.delete_icon),
+            tint = Color.White
+        )
     }
 }
 
--- app/src/main/java/com/example/todoappjetpackcompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/todoappjetpackcompose/ui/screens/list/ListScreen.kt
@@ -88,6 +88,10 @@ fun ListScreen(
                 navigateToTaskScreen = navigateToTaskScreen,
                 highPriorityTasks = highPriorityTask,
                 lowPriorityTasks = lowPriorityTask,
+                onSwipeToDelete = { action, task ->
+                    sharedViewModel.action.value = action
+                    sharedViewModel.updateTaskFields(selectedTask = task)
+                },
                 sortStates = sortState
             )
         },
